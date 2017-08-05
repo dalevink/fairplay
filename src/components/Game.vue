f3fb732f79f68c0e77f2a579c271f5d501337f5b
dalevink <dalevink@gmail.com>
2017-08-06 11:35:25 +1200

    Sort Players by time played + Added visual log of Recent Subs

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -5,73 +5,131 @@
         <div class="clear"></div>
 
         <h1>Game</h1>
+
         <transition-group name="anim-list" tag="ul" class="players">
             <li class="player"
                 v-for="player in playersFiltered"
                 :key="player.id"
-                :class="{ 'player-on': player.isOn  }"
+                :class="{ 'player-on': player.isOn === 1  }"
                 @click="clickPlayer(player)"
             >
-
                 <strong class="player-name">{{ player.name }}</strong>
-                <span class="player-seconds">{{ player.secondsOn }}</span>
+                <span class="player-seconds">{{ player.secondsOn | formatTime }}</span>
             </li>
         </transition-group>
+
+        <section v-show="logList.length">
+            <h3>Recent Sub{{ logList.length | pluralize }}</h3>
+            <transition-group name="anim-list" tag="ul" class="sub-log">
+                <li class="log"
+                    v-for="log in logList"
+                    :key="logList[0]"
+                >
+                    <strong class="player-name">{{ log[2].name }} {{ log | what }}</strong>
+                </li>
+            </transition-group>
+        </section>
     </div>
 </template>
 
 <script>
   import naturalSort from 'javascript-natural-sort'
+
+  var padLeft = function (string, pad, length) {
+    return (new Array(length + 1).join(pad) + string).slice(-length)
+  }
+  function dateRounded (syncWith) {
+    let now = new Date().getTime()
+    // Get time difference and round to nearest second
+    let dif = Math.round((now - syncWith) / 1000) * 1000
+    return new Date(syncWith + dif)
+  }
+  const states = {
+    '-1': 'On, Starts',
+    '0': 'Off',
+    '1': 'On'
+  }
+  var formatTime = function (secs) {
+    let min = Math.floor(secs / 60)
+    let sec = secs - min * 60
+    return min + ':' + padLeft(sec, '0', 2)
+  }
+
   export default {
     name: 'game',
     data () {
       return {
+        timeSync: 0,
+        logId: 0,
+        logList: [],
         players: [
         ]
       }
     },
+    beforeCreate () {
+      if (this.timeSync === 0) {
+        this.timeSync = new Date().getTime()
+      }
+    },
+
     computed: {
       playersFiltered () {
         let off = this.players.filter(function (p) {
-          return p.isOn === false
-        }).sort(naturalSort)
+          return p.isOn !== 1
+        }).sort(this.timeSort)
         let on = this.players.filter(function (p) {
-          return p.isOn !== false
-        }).sort(naturalSort)
+          return p.isOn === 1
+        }).sort(this.timeSort)
         return off.concat(on)
+      },
+      logMax () {
+        return this.logList.splice(6)
+      }
+    },
+    filters: {
+      formatTime: function (secs) {
+        return formatTime(secs)
+      },
+      pluralize: function (n) {
+        return n === 1 ? '' : 's'
+      },
+      what: function (log) {
+        let howLong = log[3] > 10 ? formatTime(log[3]) : ''
+        return states[log[1]] + ' ' + howLong
       }
     },
     methods: {
+      timeSort (a, b) {
+        return a.secondsOn < b.secondsOn ? 1 : -1
+      },
       updateData () {
-        var now = new Date()
+        var now = dateRounded(this.timeSync)
         this.players.forEach((v, i) => {
           let total = 0
-          if (v.isOn) {
+          if (v.isOn === 1) {
             total += this.secDif(v.start, now)
           }
           total += v.totalSecondsOn
-          v.secondsOn = this.formatTime(total)
+          v.secondsOn = total
         })
-      },
-      formatTime (dif) {
-        let min = Math.floor(dif / 60)
-        let sec = dif - min * 60
-        return min + ':' + this.padLeft(sec, '0', 2)
-      },
-      padLeft (string, pad, length) {
-        return (new Array(length + 1).join(pad) + string).slice(-length)
       },
       secDif (start, end) {
         return Math.floor((end - start) / 1000)
       },
       clickPlayer (player) {
-        if (player.isOn) {
-          player.totalSecondsOn += this.secDif(player.start, new Date())
-          player.start = 0
-          player.isOn = false
+        let dif = 0
+        // Do not dif first sub
+        let firstSub = player.isOn === -1
+        dif = this.secDif(player.start, dateRounded(this.timeSync))
+        if (player.isOn === 1) {
+          player.totalSecondsOn += dif
+          player.isOn = 0
         } else {
-          player.start = new Date()
-          player.isOn = true
+          player.isOn = 1
+        }
+        player.start = dateRounded(this.timeSync)
+        if (firstSub || dif > 5) {
+          this.logList.unshift([ this.logId++, firstSub ? -1 : player.isOn, player, dif ])
         }
       }
     },
@@ -90,12 +148,11 @@
       players.forEach((name, id) => {
         this.players.push({
           id: id,
-          state: 'off',
           name: name,
-          start: 0,
+          start: dateRounded(this.timeSync),
           secondsOn: 0,
           totalSecondsOn: 0,
-          isOn: false
+          isOn: -1
         })
       })
       this.updateData()
