915167dcaf994d1cf50a34239aab734c90a664d9
dalevink <dalevink@gmail.com>
2020-06-09 12:41:04 +1200

    new deploy scheme

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,7 +4,7 @@ set -e # stop on error
 echo build client ...
 npm run build
 
-echo check out branch gh-pages ...
-git subtree push --prefix dist origin gh-pages
+echo push..
+gh-pages -d dist
 
 echo All done!
